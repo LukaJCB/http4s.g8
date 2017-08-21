94917721441a9d95829bc5fc355ef7db7d8f1710
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2017-08-21 18:15:07 +0200

    Remove deprecated ExecutionService

--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -14,12 +14,10 @@ object BlazeExample extends ServerApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
   val ip   : String           = "0.0.0.0"
-  val pool : ExecutorService  = Executors.newCachedThreadPool()
 
   override def server(args: List[String]): Task[Server] =
     BlazeBuilder
       .bindHttp(port, ip)
       .mountService(HelloWorld.service)
-      .withServiceExecutor(pool)
       .start
 }
